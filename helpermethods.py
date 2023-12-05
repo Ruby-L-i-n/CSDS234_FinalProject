b6cb742bdf95ec6dc3c37bacc06c97a59c3d5498
crystal-zhu <cxz630@case.edu>
2023-12-04 23:11:18 -0500

    Update helpermethods.py
    Making the helper method for creating the binary list more general

--- helpermethods.py
+++ helpermethods.py
@@ -3,24 +3,24 @@ import pandas as pd
 import random
 
 
-def is_song_in_playlist(songs, song_in_pl_matrix):
+def is_element_in_playlist(element, element_in_pl_matrix):
 """
-A helper method to convert a list of songs into a binary list that details if the song is in the playlist or not
+A helper method to convert a list of elements into a binary list that details if that element is in the playlist or not
 
 ARGUMENTS:
 
-songs: the song titles from the matrix that tracks if songs are in playlists
-song_in_pl_matrix: a sparse matrix that tracks which songs are in which playlists
+element: the element ids/names from the matrix to record if that element is in a playlist
+element_in_pl_matrix: a sparse matrix that tracks which elements are in which playlists
 
 OUTPUTS:
-binary_pl: the binary list with 1's at pids corresponding to the songs
+binary_pl: the binary list with 1's at pids corresponding to the element
 """
 
-binary_pl = np.zeros(len(list(song_pl_df.columns)))
-song_index = [list(song_in_pl_matrix).index(s) for s in songs]
-binary_pl[song_index] = 1
+binary_pl = np.zeros(len(list(element_pl_df.columns)))
+element_index = [list(element_in_pl_matrix).index(s) for s in element]
+binary_pl[element_index] = 1
 return binary_pl
 
-# def get_titles(song_list,)
+# def get_titles(element_list,)
 
 
